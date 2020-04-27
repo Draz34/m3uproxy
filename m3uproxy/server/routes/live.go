222b09cb6e50d67f7e64c43c106c4adb84f9b976
Rodolphe ROQUES <rodolphe.roques@bipinfopro.com>
2020-04-27 16:08:28 +0200

    change live channel

--- m3uproxy/server/routes/live.go
+++ m3uproxy/server/routes/live.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	"strings"
 
 	"github.com/Draz34/m3uproxy/config"
 	"github.com/Draz34/m3uproxy/db"
@@ -14,10 +13,10 @@ import (
 
 func LiveRoute(config *config.Config) (string, func(w http.ResponseWriter, r *http.Request)) {
 
-	return "/live/{username}/{password}/{id}.{ext}", func(w http.ResponseWriter, r *http.Request) {
+	return "/live/{username}/{password}/{id}", func(w http.ResponseWriter, r *http.Request) {
 
 		vars := mux.Vars(r)
-		channelNumber := vars["id"] + "." + vars["ext"]
+		channelNumber := vars["id"]
 		username := vars["username"]
 		password := vars["password"]
 
@@ -29,15 +28,8 @@ func LiveRoute(config *config.Config) (string, func(w http.ResponseWriter, r *ht
 		channel, err := db.LookupChannel(channelNumber)
 
 		var urlIptv string = "http://" + config.Xtream.Hostname + ":" + strconv.Itoa(int(config.Xtream.Port)) + "/live/" + config.Xtream.Username + "/" + config.Xtream.Password + "/" + channelNumber
-		var urlIptvTs string = "http://" + config.Xtream.Hostname + ":" + strconv.Itoa(int(config.Xtream.Port)) + "/live/" + config.Xtream.Username + "/" + config.Xtream.Password + "/" + vars["id"] + ".ts"
 		if err != nil {
-			lastUrlIptv := webutils.TracingRedirect(urlIptvTs)
-
-			if vars["ext"] == "m3u8" {
-				log.Printf("urlIptv url now : %s", urlIptv)
-				lastUrlIptv = strings.Replace(urlIptv, ".ts", ".m3u8", -1)
-				urlIptv = lastUrlIptv
-			}
+			webutils.TracingRedirect(urlIptv)
 
 			log.Printf("Register Channel for %s", urlIptv)
 			channel, _ = db.RegisterChannel(urlIptv)
